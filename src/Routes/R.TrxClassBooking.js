93604385af0a544d1a2d059fd7985d2655ba2133
Fattan-malva <fattanmalva14@gmail.com>
2025-11-02 21:53:37 +0700

    [UP]Bearer Token

--- src/Routes/R.TrxClassBooking.js
+++ src/Routes/R.TrxClassBooking.js
@@ -4,9 +4,9 @@ const BookingController = require('../Controllers/C.TrxClassBooking');
 const { authenticateToken } = require('../Auth/middleware');
 
 
-router.get('/', BookingController.index);
-router.get('/find-by-uniq-code', BookingController.findByUniqCode);
-router.get('/find-by-customer-id', BookingController.findByCustomerID);
+router.get('/', authenticateToken, BookingController.index);
+router.get('/find-by-uniq-code',authenticateToken, BookingController.findByUniqCode);
+router.get('/find-by-customer-id', authenticateToken, BookingController.findByCustomerID);
 router.post('/', authenticateToken, BookingController.create);
 
 module.exports = router;
