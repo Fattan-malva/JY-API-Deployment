f56d2b1c02c8c4eacf50345cdc3294eef3baf718
Fattan-malva <fattanmalva14@gmail.com>
2025-11-04 01:01:31 +0700

    [UP]Validation

--- src/Routes/R.TrxStudentJM_BookingList.js
+++ src/Routes/R.TrxStudentJM_BookingList.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const TrxStudentJM_BookingListController = require('../Controllers/C.TrxStudentJM_BookingList');
+const { authenticateToken } = require('../Auth/middleware');
 
 
-router.get('/find-by-customer-id', TrxStudentJM_BookingListController.show);
+router.get('/find-by-customer-id', authenticateToken, TrxStudentJM_BookingListController.show);
 
 module.exports = router;
