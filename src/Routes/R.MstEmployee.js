6cf70a950caf15aa31f99b2b4454723a3896c106
Fattan-malva <fattanmalva14@gmail.com>
2025-11-08 22:12:02 +0700

    [UP]MstEmployee

--- src/Routes/R.MstEmployee.js
+++ src/Routes/R.MstEmployee.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const EmployeeController = require('../Controllers/C.MstEmployee');
+const { authenticateToken } = require('../Auth/middleware');
 
 // Public (tidak perlu login)
-router.get('/', EmployeeController.index);
+router.get('/', authenticateToken, EmployeeController.index);
 
 module.exports = router;
