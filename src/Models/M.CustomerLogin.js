01e12d1dedd3b02a3f5dd08f472c5f767dca7be6
Fattan-malva <fattanmalva14@gmail.com>
2025-11-20 22:58:40 +0700

    [ADD]EndDateMembership

--- src/Models/M.CustomerLogin.js
+++ src/Models/M.CustomerLogin.js
@@ -15,6 +15,8 @@ async function findById(customerID) {
         mcl.lastContractID,
         mp.isAllClub,
         tc.status,
+		    tc.endDate As EndDateMembership,
+		    tjm.endDate As EndDateJMMembership,
         mcl.noIdentity,
         mcl.birthDate,
         mcl.phone,
@@ -22,9 +24,10 @@ async function findById(customerID) {
       FROM MstCustomerLogin mcl
 	      INNER JOIN MstStudio ms ON ms.studioID = mcl.toStudioID
         LEFT JOIN TrxContract tc ON tc.contractID = mcl.lastContractID
+		    LEFT JOIN TrxJustMe tjm ON tjm.customerID = mcl.customerID
         LEFT JOIN MstProduct mp ON mp.productID = tc.productID
         LEFT JOIN MstCustomerImage mci ON mci.customerID = mcl.customerID
-      WHERE mcl.customerID =  @customerID`);
+      WHERE mcl.customerID = @customerID`);
   return result.recordset[0];
 }
 
