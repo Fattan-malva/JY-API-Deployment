ceebf2e631e1e9b469cb3154ce9da0923498ca15
Fattan-malva <fattanmalva14@gmail.com>
2025-12-15 16:42:53 +0700

    [UP]Premission

--- src/Routes/R.CustomerLogin.js
+++ src/Routes/R.CustomerLogin.js
@@ -4,8 +4,8 @@ const CustomerLoginController = require('../Controllers/C.CustomerLogin');
 const { authenticateToken } = require('../Auth/middleware');
 
 // Public route to get customer by ID
-router.get('/',  CustomerLoginController.show);
-router.get('',  CustomerLoginController.show);
+router.get('/', authenticateToken, CustomerLoginController.show);
+router.get('', authenticateToken, CustomerLoginController.show);
 
 module.exports = router;
     
